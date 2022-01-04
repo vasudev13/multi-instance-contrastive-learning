10f7c3e7bb8867df680b6ba45804dcfd292df267
Vasudev Awatramani <30664477+vasudev13@users.noreply.github.com>
2022-01-04 03:00:08 -0500

    Update and rename atelectasis.py to downstream.py

--- downstream.py
+++ downstream.py
@@ -26,67 +26,67 @@ wandb.config = {
   "epochs": 2,
   "batch_size": 32
 }
-# SEED = 13
-# set_all_seeds(SEED)
-# PATH = '/scratch/va2134/densenet121_encoder.ckpt' 
-# state_dict = torch.load(PATH)
-# model = densenet.densenet121(pretrained=False)
-# model.classifier = nn.Identity()
-# model.load_state_dict(state_dict)
-# model.classifier = nn.Linear(in_features=1024, out_features=5, bias=True)
-# model.cuda()
+SEED = 13
+set_all_seeds(SEED)
+PATH = '/scratch/va2134/densenet121_encoder.ckpt' 
+state_dict = torch.load(PATH)
+model = densenet.densenet121(pretrained=False)
+model.classifier = nn.Identity()
+model.load_state_dict(state_dict)
+model.classifier = nn.Linear(in_features=1024, out_features=5, bias=True)
+model.cuda()
 
-# root = '/scratch/va2134/datasets/CheXpert-v1.0-small/'
-# Index: -1 denotes multi-label mode including 5 diseases
-# traindSet = CheXpert(csv_path=root+'train.csv', image_root_path=root, use_upsampling=False, use_frontal=True, image_size=224, mode='train', class_index=-1)
-# testSet =  CheXpert(csv_path=root+'valid.csv',  image_root_path=root, use_upsampling=False, use_frontal=True, image_size=224, mode='valid', class_index=-1)
-# trainloader =  torch.utils.data.DataLoader(traindSet, batch_size=32, num_workers=2, shuffle=True)
-# testloader =  torch.utils.data.DataLoader(testSet, batch_size=32, num_workers=2, shuffle=False)
+root = '/scratch/va2134/datasets/CheXpert-v1.0-small/'
+Index: -1 denotes multi-label mode including 5 diseases
+traindSet = CheXpert(csv_path=root+'train.csv', image_root_path=root, use_upsampling=False, use_frontal=True, image_size=224, mode='train', class_index=-1)
+testSet =  CheXpert(csv_path=root+'valid.csv',  image_root_path=root, use_upsampling=False, use_frontal=True, image_size=224, mode='valid', class_index=-1)
+trainloader =  torch.utils.data.DataLoader(traindSet, batch_size=32, num_workers=2, shuffle=True)
+testloader =  torch.utils.data.DataLoader(testSet, batch_size=32, num_workers=2, shuffle=False)
 
-# # paramaters
-# BATCH_SIZE = 32
-# lr = 1e-4
-# weight_decay = 1e-5
+# paramaters
+BATCH_SIZE = 32
+lr = 1e-4
+weight_decay = 1e-5
 
-# # define loss & optimizer
-# CELoss = CrossEntropyLoss()
-# optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+# define loss & optimizer
+CELoss = CrossEntropyLoss()
+optimizer = Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
-# # training
-# best_val_auc = 0 
-# for epoch in range(1):
-#     for idx, data in enumerate(trainloader):
-#       train_data, train_labels = data
-#       train_data, train_labels  = train_data.cuda(), train_labels.cuda()
-#       y_pred = model(train_data)
-#       loss = CELoss(y_pred, train_labels)
-#       optimizer.zero_grad()
-#       loss.backward()
-#       optimizer.step()
+# training
+best_val_auc = 0 
+for epoch in range(1):
+    for idx, data in enumerate(trainloader):
+      train_data, train_labels = data
+      train_data, train_labels  = train_data.cuda(), train_labels.cuda()
+      y_pred = model(train_data)
+      loss = CELoss(y_pred, train_labels)
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
         
-#       # validation  
-#       if idx % 400 == 0:
-#          model.eval()
-#          with torch.no_grad():    
-#               test_pred = []
-#               test_true = [] 
-#               for jdx, data in enumerate(testloader):
-#                   test_data, test_labels = data
-#                   test_data = test_data.cuda()
-#                   y_pred = torch.sigmoid(model(test_data))
-#                   test_pred.append(y_pred.cpu().detach().numpy())
-#                   test_true.append(test_labels.numpy())
+      # validation  
+      if idx % 400 == 0:
+         model.eval()
+         with torch.no_grad():    
+              test_pred = []
+              test_true = [] 
+              for jdx, data in enumerate(testloader):
+                  test_data, test_labels = data
+                  test_data = test_data.cuda()
+                  y_pred = torch.sigmoid(model(test_data))
+                  test_pred.append(y_pred.cpu().detach().numpy())
+                  test_true.append(test_labels.numpy())
             
-#               test_true = np.concatenate(test_true)
-#               test_pred = np.concatenate(test_pred)
-#               val_auc_mean =  roc_auc_score(test_true, test_pred) 
-#               model.train()
+              test_true = np.concatenate(test_true)
+              test_pred = np.concatenate(test_pred)
+              val_auc_mean =  roc_auc_score(test_true, test_pred) 
+              model.train()
 
-#               if best_val_auc < val_auc_mean:
-#                  best_val_auc = val_auc_mean
-#                  torch.save(model.state_dict(), '/scratch/va2134/ce_pretrained_model.pth')
+              if best_val_auc < val_auc_mean:
+                 best_val_auc = val_auc_mean
+                 torch.save(model.state_dict(), '/scratch/va2134/ce_pretrained_model.pth')
 
-#               print ('Epoch=%s, BatchID=%s, Val_AUC=%.4f, Best_Val_AUC=%.4f'%(epoch, idx, val_auc_mean, best_val_auc ))
+              print ('Epoch=%s, BatchID=%s, Val_AUC=%.4f, Best_Val_AUC=%.4f'%(epoch, idx, val_auc_mean, best_val_auc ))
 
 ## Edema Pre-training
 
@@ -100,16 +100,12 @@ trainloader =  torch.utils.data.DataLoader(traindSet, batch_size=32, num_workers
 testloader =  torch.utils.data.DataLoader(testSet, batch_size=32, num_workers=2, shuffle=False)
 
 # paramaters
-SEED = 13
 BATCH_SIZE = 32
 imratio = traindSet.imratio
 lr = 0.05 # using smaller learning rate is better
 gamma = 500
 weight_decay = 1e-5
 margin = 1.0
-
-# model
-set_all_seeds(SEED)
 
 # load pretrained model
 if True:
